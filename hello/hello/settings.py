df948942e44a90d58a652fcc0d4eba0bd09c5930
softarf <98207887+softarf@users.noreply.github.com>
2023-04-16 12:58:34 +0500

    Меняет язык окна приветствия на русский.
    3.3 Первый проект на Django. стр 97.
    Changes committed:
        1. modified:   hello/hello/settings.py

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -103,7 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'    # Установить для окна приветствия русский язык.
 
 TIME_ZONE = 'UTC'
 
