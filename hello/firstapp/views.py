93850f8d741d953b595569313478fbf230d0679a
softarf <98207887+softarf@users.noreply.github.com>
2023-05-11 21:41:07 +0500

    Вносит косметические исправления.
    Changes committed:
        1. modified:   hello/firstapp/views.py
        2. modified:   hello/hello/settings.py
        3. modified:   hello/requirements.txt
        4. deleted:    hello/.gitignore

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 
@@ -6,29 +5,34 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 # Create your views here.
 
 def index(request):
+    # return HttpResponse("<h2>Главная</h2>")
     return render(request, "index.html")
+
 
 def about(request):
     return HttpResponse("<h2>О сайте</h2>")
 
+
 def contact(request):
     # return HttpResponse("<h2>Контакты</h2>")
-    return HttpResponseRedirect('/about')    # Временная переадресация на страницу "О сайте".
-                                             # Косая черта (/) обязательно должна присутствовать в начале пути.
+    #                                        Временная переадресация на страницу "О сайте".
+    return HttpResponseRedirect('/about')  # Косая черта (/) обязательно должна стоять в начале пути.
+
 
 def products(request, product_id=1):
     category = request.GET.get("cat", "")
-    output = f"<h2>Продукт № {product_id} Категория: {category}</h2>"
     # output = f"<h2>Продукт № {product_id}</h2>"
+    output = f"<h2>Продукт № {product_id} Категория: {category}</h2>"
     return HttpResponse(output)
 
-def users(request):              # Убрали параметры представления - ", user_id=1, name='Андрей'".
+
+def users(request):    # Убрали параметры представления - ", user_id=1, name='Андрей'".
     user_id = request.GET.get("user_id", 1)
     name = request.GET.get("name", "Андрей")
     output = f"<h2>Пользватель</h2><h3>id: {user_id} Имя: {name}</h3>"
-    # output = f"<h2>Пользватель</h2><h3>id: {user_id} Имя: {name}</h3>"
     return HttpResponse(output)
+
 
 def details(request):
     # return HttpResponse("<h2>Контакты</h2>")
-    return HttpResponsePermanentRedirect('/')    # Постоянная переадресация на страницу "Главная".
+    return HttpResponsePermanentRedirect('/')  # Постоянная переадресация на страницу "Главная".
